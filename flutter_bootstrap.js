8e76bd106f38fa71a8d8a9d378740d41a0f40225
Felix Magnell <felix.magnell@airahome.com>
2025-02-22 23:55:32 +0100

    updated games

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2246589786"
+    serviceWorkerVersion: "3813199453"
   }
 });
